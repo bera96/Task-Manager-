490f8828250e04a1b2400b417a3cd8c2f6ba2885
Bera Yılmazyurt <sentinelprime33@gmail.com>
2022-06-20 04:47:00 +0300

    first commit

---
+++ db/connect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+};
+
+module.exports = connectDB
